9f56ef3aed009a5c7a092927c25ffd7ce768b99e
Max Battcher <me@worldmaker.net>
2015-01-15 00:04:07 -0500

    Add URL to custom Disqus vars; attempt resets

--- StingerCheck/App/widgets/disqus/viewmodel.ts
+++ StingerCheck/App/widgets/disqus/viewmodel.ts
@@ -1,4 +1,6 @@
-﻿class DisqusVm {
+﻿declare var DISQUS: any;
+
+class DisqusVm {
     identifier: string;
     title: string;
 
@@ -11,7 +13,22 @@
         /* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
         var disqus_shortname = (<any>window).disqus_shortname = 'stingercheck'; // required: replace example with your forum shortname
         var disqus_identifier = (<any>window).disqus_identifier = this.identifier;
+        var disqus_url = (<any>window).disqus_url = window.location.href;
         var disqus_title = (<any>window).disqus_title = this.title;
+
+        // DISQUS reset pulled from StackOverflow
+        //    http://stackoverflow.com/questions/21903875/same-disqus-thread-is-loading-for-all-articles
+        //    https://stackoverflow.com/questions/8944287/disqus-loading-the-same-comments-for-dynamic-pages?rq=1
+        if (typeof DISQUS != 'undefined') {
+            DISQUS.reset({
+                reload: true,
+                config: function () {
+                    this.page.identifier = disqus_identifier;
+                    this.page.url = disqus_url;
+                    this.page.title = disqus_title;
+                }
+            });
+        } else {
 
             /* * * DON'T EDIT BELOW THIS LINE * * */
             (function () {
@@ -19,6 +36,8 @@
                 dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
                 (<any>document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
             })();
+
+        }
     }
 }
 
